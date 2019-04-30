e3e5cd26e386776d775f22448122bd56522d4aad
Buzz <Buzz627@gmail.com>
2019-04-30 11:53:58 -0400

    changed port

--- serverTest.js
+++ serverTest.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 4000
+var PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World! test'))
 
-app.listen(port, "0.0.0.0", () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
